1b1c1ef3e19004af351cfbfec366e6dc1747a0cc
philippe SIMO <philippechampion58@gmail.com>
2019-01-18 10:06:32 +0100

    feat: first commit: Base Domain driven architecture

---
+++ src/main/java/com/doranco/snapanonyme/SnapanonymeApplication.java
@@ -0,0 +1,14 @@
+package com.doranco.snapanonyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapanonymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapanonymeApplication.class, args);
+	}
+
+}
+
